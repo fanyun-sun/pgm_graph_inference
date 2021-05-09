add arguments and fix kl bug

inference/maxbpnn.py
@@ -187,7 +187,7 @@ def build_FactorGraph_from_BMRF(J, b):
 class MaxBPNN(nn.Module):
     def __init__(self, mode='map', damping=0., normalization_flag=True,
                  hidden_size=64, batch_norm_flag=True,
-                 maxiter=10000, tol=1e-9,
+                 maxiter=10000, tol=1e-20,
                  src_names=['diff_f2v_messages', 'diff_beliefs'],):
         super(MaxBPNN, self).__init__()
         if mode not in ["marginal", "map"]:
@@ -208,13 +208,13 @@ class MaxBPNN(nn.Module):
 class MaxBPNNInference(GatedGNNInference):
     def __init__(self, mode, state_dim, message_dim,
                 hidden_unit_message_dim, hidden_unit_readout_dim,
-                n_steps=10, load_path=None, sparse=True):
+                n_steps=10, load_path=None, sparse=True, damping=.5, normalize=False):
         Inference.__init__(self, mode)
 
-        normalization_flag=True
-        damping=.9999
-        print('normalization_flag', normalization_flag)
-        print('damping', damping)
+        normalization_flag=normalize
+        damping=damping
+        with open('results.log', 'a+') as f:
+            f.write('{},{}\n'.format(normalization_flag, damping))
 
         self.model = MaxBPNN(
             mode=mode,

run_exps.py
@@ -82,26 +82,27 @@ def run_experiment(train_set_name, test_set_name, inference_mode="marginal",
     """
     tests for in-sample (same structure, same size, marginals)
     """
-    train_path = os.path.join(base_data_dir, "train")
+    # train_path = os.path.join(base_data_dir, "train")
     test_path = os.path.join(base_data_dir, "test")
-     
-    model_load_path = os.path.join(model_base_dir, args.model_name + '-' +  train_set_name)
 
-    train_data = get_dataset_by_name(train_set_name, train_path)
+    # model_load_path = os.path.join(model_base_dir, args.model_name + '-' +  train_set_name)
+    model_load_path = os.path.join(model_base_dir, args.model_name + '-' + train_set_name + str(args.normalize) + '-' + str(args.damping))
+
+    # train_data = get_dataset_by_name(train_set_name, train_path)
     test_data  = get_dataset_by_name(test_set_name, test_path, mode=inference_mode)
 
     print('load model from {}'.format(model_load_path))
-    print('load train data from {}/{}'.format(train_path, train_set_name))
+    # print('load train data from {}/{}'.format(train_path, train_set_name))
     print('load test data from {}/{}'.format(test_path, test_set_name))
- 
+
     # load model
     gnn_constructor = get_algorithm(args.model_name)
-    gnn_inference = gnn_constructor(inference_mode, n_hidden_states, 
+    gnn_inference = gnn_constructor(inference_mode, n_hidden_states,
                                     message_dim_P,hidden_unit_message_dim,
                                     hidden_unit_readout_dim, T,
-                                    model_load_path, USE_SPARSE_GNN)
+                                    model_load_path, USE_SPARSE_GNN,
+                                    normalize=args.normalize,damping=args.damping)
     print('model loaded from {}'.format(model_load_path))
-
     # run inference on test
     times = {}
 
@@ -204,6 +205,8 @@ def parse_exp_args():
                         help='name of experiment to run')
     parser.add_argument('--model_name', default='default',
                         type=str, help='model name, defaults to the train_set_name')
+    parser.add_argument('--normalize', default=False, action='store_true')
+    parser.add_argument('--damping', default=.99, type=float)
     args = parser.parse_args()
     return args
 
@@ -214,21 +217,29 @@ def kl_div(p, q):
     p, q : array-like, dtype=float, shape=n
     Discrete probability distributions.
     """
-    p = np.asarray(p, dtype=float)
-    q = np.asarray(q, dtype=float)
-
-    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
+    kls = []
+    for p, q in zip(p, q):
+        p = np.array([1.-p, p])
+        q = np.array([1.-q, q])
+        kl = np.sum(np.where(p != 0., p * np.log(p / q), 0))
+        kls.append(kl)
+    return np.mean(kls)
+    # p = np.asarray(p, dtype=float)
+    # q = np.asarray(q, dtype=float)
+    # return np.sum(np.where(p != 0., p * np.log(p / q), 0))
 
 def save_marginal_results(true_labels, gnn_labels, bp_labels, mcmc_labels, filename, colors=None):
     res = {'true_labels': true_labels, 'gnn_labels': gnn_labels, 'bp_labels': bp_labels,
             'mcmc_labels': mcmc_labels, 'colors': colors}
+    print('len(true_labels)', len(true_labels))
     for k, v in res.items():
         if k == 'colors':
             continue
 
-        kl = kl_div(np.array(true_labels), np.array(v))*2/len(true_labels)
+        kl = kl_div(true_labels, v)
         print('{}, KL: {:.5f}, RMSE: {:.5f}'.format(k, kl, np.sqrt(((np.array(true_labels) - np.array(v))**2).mean())))
-    np.save(filename, res, allow_pickle=True)
+    np.save(filename, res)
+    # np.save(filename, res)
 
 def plot_marginal_results_individual(true_labels, gnn_labels, bp_labels, mcmc_labels, filename):
     fsize=(10,10)

train.py
@@ -30,7 +30,7 @@ class CrossEntropyComputer:
 class KLDivLossComputer:
     def __init__(self):
         self.computer = nn.KLDivLoss()
-    
+
     def __call__(self, output_probs, targets):
         logits = torch.log(output_probs)
         return self.computer(logits, targets)
@@ -54,8 +54,9 @@ def parse_train_args():
     parser.add_argument('--epochs', default=1, type=int,
                         help='number of epochs to train for')
 
-    # non-critical arguments, fine with default
-    # model_name can be used for different hyperparameters later
+    parser.add_argument('--normalize', default=False, action='store_true')
+    parser.add_argument('--damping', default=.99, type=float) 
+    # non-critical arguments, fine with default model_name can be used for different hyperparameters later
     parser.add_argument('--model_name', default='default',
                         type=str, help='model name, defaults to the train_set_name')
     parser.add_argument('--data_dir', default='./graphical_models/datasets/train',
@@ -79,7 +80,7 @@ if __name__ == "__main__":
     dataset = get_dataset_by_name(args.train_set_name, args.data_dir, 
                                   mode=args.mode)
     os.makedirs(args.model_dir, exist_ok=True)
-    model_path = os.path.join(args.model_dir, args.model_name + '-' + args.train_set_name)
+    model_path = os.path.join(args.model_dir, args.model_name + '-' + args.train_set_name + str(args.normalize) + '-' + str(args.damping))
 
     # # filter by mode:
     # if args.mode =="marginal":
@@ -104,7 +105,8 @@ if __name__ == "__main__":
         gnn_constructor = get_algorithm(args.model_name)
     gnn_inference = gnn_constructor(args.mode, n_hidden_states, message_dim_P,
                                     hidden_unit_message_dim, hidden_unit_readout_dim,
-                                    T, sparse=USE_SPARSE_GNN)
+                                    T, sparse=USE_SPARSE_GNN, normalize=args.normalize,
+                                    damping=args.damping)
     if args.use_pretrained != 'none':
         model_path_pre = os.path.join(args.model_dir, args.use_pretrained)
         gnn_inference.load_model(model_path_pre)
@@ -120,23 +122,23 @@ if __name__ == "__main__":
     else:
         criterion = CrossEntropyMAPComputer()
 
-    gnn_inference.save_model(model_path)
-    print("Model saved in {}".format(model_path))
+    # gnn_inference.save_model(model_path)
+    # print("Model saved in {}".format(model_path))
 
     ### uncomment the following if you want the model to be trained ###
 
-    # best_epoch = -1
-    # best_loss = 1e9
-    # for epoch in range(args.epochs):
-        # gnn_inference.train(dataset, optimizer, criterion, DEVICE)
-        # loss = gnn_inference.history["loss"][-1]
+    best_epoch = -1
+    best_loss = 1e9
+    for epoch in range(args.epochs):
+        gnn_inference.train(dataset, optimizer, criterion, DEVICE)
+        loss = gnn_inference.history["loss"][-1]
 
-        # if loss < best_loss:
-            # best_loss = loss
-            # best_epoch = epoch
-            # gnn_inference.save_model(model_path)
-            # print('Epoch {}: loss {}'.format(epoch, loss))
-            # print("Model saved in {}".format(model_path))
+        if loss < best_loss:
+            best_loss = loss
+            best_epoch = epoch
+            gnn_inference.save_model(model_path)
+            print('Epoch {}: loss {}'.format(epoch, loss))
+            print("Model saved in {}".format(model_path))
 
-        # if epoch - best_epoch > 4:
-            # break
+        if epoch - best_epoch > 4:
+            break