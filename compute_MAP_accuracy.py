b72baa9f265f12e7191ef070aab70a4614452f07
Fan-Yun Sun <sunfanyun@gmail.com>
2023-12-12 14:12:23 -0800

    update compute_MAP_accuracy.py

--- compute_MAP_accuracy.py
+++ compute_MAP_accuracy.py
@@ -1,6 +1,7 @@
 import numpy as np
 import argparse
 
+
 def parse_train_args():
     parser = argparse.ArgumentParser()
 
@@ -12,18 +13,15 @@ def parse_train_args():
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
     args = parse_train_args()
     print('load results from {}'.format(args.data_file))
     data = np.load(args.data_file, allow_pickle=True).item(0)
-    # print(data.keys())
-
-    # print(data['true_labels'])
-
+    
     MAP_true = np.array(data['true_labels']) > args.map_threshold
     number_of_samples = MAP_true.shape[0]
-
-
+    
     GNN_true = np.array(data['gnn_labels']) > args.map_threshold
     print('GNN MAP Accuracy: ', np.sum(GNN_true==MAP_true)/number_of_samples)
 
@@ -34,4 +32,3 @@ if __name__ == "__main__":
     if('mcmc_labels' in data):
         MCMC_true = np.array(data['mcmc_labels']) > args.map_threshold
         print('MCMC MAP Accuracy: ', np.sum(MCMC_true==MAP_true)/number_of_samples)
-
